25e2438b36c65f8683c3542e0ce63000c16f10a4
Pei Tao <peitao@Peis-MacBook-Pro.local>
2015-01-13 14:26:54 -0500

    basic first half of form

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET input form page. */
+router.get('/form1', function(req, res) {
+  res.render('form1', { title: 'Form' });
+});
+
 module.exports = router;
