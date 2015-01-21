e8a57672e023c7bbc8d008985b80ff6b23626f31
cliu314 <clareliu@mit.edu>
2015-01-21 12:28:44 -0500

    only 1 map appears now

--- public/javascripts/filter.js
+++ public/javascripts/filter.js
@@ -1,10 +1,24 @@
 $(document).ready(function(){
 	var data = {}
+			var w = 750;
+			var h = 450;
+			//Define map projection
+			var projection = d3.geo.albersUsa()
+			.translate([w/2, h/2]).scale([1000]);
+			//Define path generator
+			var path = d3.geo.path()
+			.projection(projection);
+			//Create SVG element
+			svg = d3.select("body")
+			.append("svg")
+			.attr("width", w)
+			.attr("height", h);
+
 	$.get('/organization/', function(data){
   			//console.log(data.content);
   			createMap(data.content.message);
   		});
-	createMap(data);
+	//createMap(data);
 
 	$("input[name='funding_area']").change(function(){
 		var populations = ""
@@ -39,19 +53,7 @@ $(document).ready(function(){
 	function createMap(data){
 			//Width and height
 			console.log(data);
-			var w = 500;
-			var h = 300;
-			//Define map projection
-			var projection = d3.geo.albersUsa()
-			.translate([w/2, h/2]).scale([500]);
-			//Define path generator
-			var path = d3.geo.path()
-			.projection(projection);
-			//Create SVG element
-			var svg = d3.select("body")
-			.append("svg")
-			.attr("width", w)
-			.attr("height", h);
+
 			//Load in GeoJSON data
 			d3.json("/files/us-states.json", function(json) {
 				
@@ -103,11 +105,33 @@ $(document).ready(function(){
 
         });
 		}
+
+		function updateMap(data){
+			console.log(data);
+			console.log(svg);
+			var circles = svg.selectAll("circle")
+				.data(data)
+			circles.exit().remove();
+			circles
+				.enter()
+				.append("circle")
+				.attr("cx", function(d) {
+					console.log(d);
+					return projection([d.longitude, d.latitude])[0];
+				})
+				.attr("cy", function(d) {
+					return projection([d.longitude, d.latitude])[1];
+				})
+				.attr("r", 5)
+				.style("fill", "yellow")
+				.style("opacity", 0.75);
+			//circles.exit().remove();
+		}
 		function returnQuery(dataObj){
 			var queryString = decodeURIComponent($.param(dataObj));
 			$.get('/organization/?' + queryString, function(data){
-				//console.log(data.content);
-				createMap(data.content.message);
+				console.log(data.content);
+				updateMap(data.content.message);
 			});
 		}
 
